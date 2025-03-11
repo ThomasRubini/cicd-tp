ec5a7270703957adfcc27371ed0a38bec511e566
Proxyfil <piloupilou200@gmail.com>
2025-03-11 16:36:58 +0100

    fix : fixed .sql file and added in docker-compose

--- postgres_init/db.sql
+++ postgres_init/db.sql
@@ -1,10 +1,9 @@
 CREATE TABLE IF NOT EXISTS city (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     department_code VARCHAR(10) NOT NULL,
     insee_code VARCHAR(20),
     zip_code VARCHAR(20),
     name VARCHAR(255) NOT NULL,
     lat FLOAT NOT NULL,
-    lon FLOAT NOT NULL,
-    PRIMARY KEY (id)
+    lon FLOAT NOT NULL
 );
